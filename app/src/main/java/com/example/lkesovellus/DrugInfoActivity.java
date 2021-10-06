b2e15d6f1098a571dfc62d4fe650eb0255e30c89
Emil <emilsuuronen@gmail.com>
2021-10-06 15:42:11 +0300

    Infon matematiikka

--- app/src/main/java/com/example/lkesovellus/DrugInfoActivity.java
+++ app/src/main/java/com/example/lkesovellus/DrugInfoActivity.java
@@ -14,6 +14,7 @@ public class DrugInfoActivity extends AppCompatActivity {
     private TextView drugName;
     private TextView drugAmount;
     private TextView drugPrice;
+    private int taken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +22,18 @@ public class DrugInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_drug_info);
 
         drugName = findViewById(R.id.drugNameTextView);
+        drugAmount = findViewById(R.id.drugAmountTextView);
+        drugPrice = findViewById(R.id.drugPriceTextView);
+        taken = 0;
+
         Bundle b = getIntent().getExtras();
         int i = b.getInt(MainActivity.EXTRA, 0);
 
         drugName.setText(Global.getInstance().getDrugs().get(i).getDrugName());
+        drugAmount.setText(("Doses left: " + Global.getInstance().getDrugs().get(i).getDrugAmount()) + "/" +
+                Global.getInstance().getDrugs().get(i).getDrugAmount());
+        drugPrice.setText(Global.getInstance().getDrugs().get(i).getDrugPrice() + "€, Cost per dose:" +
+                (Global.getInstance().getDrugs().get(i).getDrugAmount() / Global.getInstance().getDrugs().get(i).getDrugPrice()) + "€");
     }
 
 
