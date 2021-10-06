08503b274f458e62f4597b0622a46205dfac7a77
Emil <emilsuuronen@gmail.com>
2021-10-06 18:39:44 +0300

    Info logiikka

--- app/src/main/java/com/example/lkesovellus/MainActivity.java
+++ app/src/main/java/com/example/lkesovellus/MainActivity.java
@@ -55,40 +55,6 @@ public class MainActivity extends AppCompatActivity {
 
         createNotificationChannel();
 
-
-        Button btSetAlarm = findViewById(R.id.btSetAlarm);
-
-        btSetAlarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Muistutus asetettu", Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(MainActivity.this, ReminderBroadcast.class);
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
-
-                AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-                long buttonClickTime = System.currentTimeMillis();
-                long dayInMillisec = 1000 * 10;
-
-                alarmManager.set(RTC_WAKEUP, buttonClickTime + dayInMillisec, pendingIntent);
-            }
-        });
-
-        Button btCancelAlarm = findViewById(R.id.btCancelAlarm);
-
-        btCancelAlarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Muistutus poistettu", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, ReminderBroadcast.class);
-                PendingIntent pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
-
-                AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-                alarmManager.cancel(pendingIntent);
-            }
-        });
-
-
     }
 
     // lisäyspainikkeen metodi joka vaihtaa aktiviteettiin drugAddActivity
